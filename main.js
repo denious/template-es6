c259bc2ebdaae298793ae6aa8e7e852f054f9c67
Denis O <denious@outlook.com>
2021-04-29 11:48:59 -0500

    Added faker and updated test script

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-import http from 'k6/http';
-import { check } from 'k6';
-import cheerio from 'cheerio';
-import xml2js from 'xml2js';
+import * as faker from "faker/locale/en_US";
 
-export default function () {
-  const res = http.get('https://loadimpact.com/');
+export const options = {
+  stages: [{ iterations: 1 }],
+};
 
-  const $ = cheerio.load(res.body);
-
-  const title = $('head title').text();
-  check(title, {
-    'has correct title': () => title == 'Load Impact is now k6',
-  });
-  console.log(title);
-
-  var xmlString =
-    '<?xml version="1.0" ?>' +
-    '<items xmlns="http://foo.com">' +
-    ' <item>Foo</item>' +
-    ' <item color="green">Bar</item>' +
-    '</items>';
-
-  xml2js.parseString(xmlString, function (err, result) {
-    console.log(JSON.stringify(err));
-    console.log(result);
-    console.log(JSON.stringify(result));
-  });
+export default function main() {
+  console.log(faker.name.firstName());
 }
