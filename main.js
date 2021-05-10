9e5212d9885e28ef8f8943c012c5a263ce82b7e4
Denis O <denious@outlook.com>
2021-05-10 11:47:07 -0500

    commit breaks k6 with addition od randomItem util

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+import { randomItem } from "https://jslib.k6.io/k6-utils/1.0.0/index.js";
 import * as faker from "faker/locale/en_US";
 
 export const options = {
